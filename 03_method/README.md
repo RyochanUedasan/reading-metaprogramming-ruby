d6ae0e1eebf4fd16b5fb6f5021e0ba30ddb77d2f
Ryohei Ueda <ueda@anipos.co.jp>
2022-05-27 14:37:12 +0900

    03_method

--- 03_method/README.md
+++ 03_method/README.md
@@ -1,3 +1,8 @@
 # Method
 
-テキストの要約をしましょう
+- define_method/method_missing
+- method_missingを使えば外部のスキーマをそのまま生かせる
+- ブランクスレートで不要なメソッドを継承しない
+- ゴーストメソッドは無数の呼び出しに対応可能
+- define_methodはクラスにインスタンスメソッドを追加する、define_singleton_methodはオブジェクトに対して特異メソッド（シングルトンメソッド）を定義する。
+
--- 03_method/define.rb
+++ 03_method/define.rb
@@ -12,7 +12,53 @@
 # - また、2で定義するメソッドは以下を満たすものとする
 #   - メソッドが定義されるのは同時に生成されるオブジェクトのみで、別のA2インスタンスには（同じ値を含む配列を生成時に渡さない限り）定義されない
 
+class A1
+  define_method("//") do
+    "//"
+  end
+end
+
+class A2
+  def initialize(arr)
+    arr.each do |a|
+      method_name = "hoge_#{a}"
+      define_singleton_method(method_name) do |count|
+        return dev_team if count == nil
+        method_name * count
+      end
+    end
+  end
+
+  def dev_team
+    "SmartHR Dev Team"
+  end
+end
+
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
+
+module OriginalAccessor
+
+  def self.included(base)
+    base.extend(ClassMethod)
+  end
+
+  module ClassMethod
+    def my_attr_accessor(arg)
+      define_method(arg) do
+        instance_variable_get("@#{arg}")
+      end
+
+      define_method("#{arg}=") do |n|
+        if [true, false].include?(n)
+          define_singleton_method("#{arg}?") do
+            !!send(arg)
+          end
+        end
+        instance_variable_set("@#{arg}", n)
+      end
+    end
+  end
+end
